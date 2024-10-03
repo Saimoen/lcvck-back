b925995310e68714f22793c4e7f5ee75c2afe5d4
Gregory SAIMOEN <gregory.saimoen@gouv.nc>
2024-10-03 13:29:44 +1100

    ajout du token JWT

--- end/src/main/java/lcvck/back/end/controller/AuthController.java
+++ end/src/main/java/lcvck/back/end/controller/AuthController.java
@@ -1,22 +1,27 @@
 package lcvck.back.end.controller;
 
+import lcvck.back.end.config.UserAuthProvider;
 import lcvck.back.end.dto.CredentialsDto;
 import lcvck.back.end.dto.UserDto;
 import lcvck.back.end.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
     private final UserService userService;
+    private final UserAuthProvider userAuthProvider;
 
     @PostMapping("/login")
     public ResponseEntity<UserDto> login (@RequestBody CredentialsDto credentialsDto) {
       UserDto user = userService.login(credentialsDto);
+      user.setToken(userAuthProvider.createToken(user));
        return ResponseEntity.ok(user);
     }
 }
